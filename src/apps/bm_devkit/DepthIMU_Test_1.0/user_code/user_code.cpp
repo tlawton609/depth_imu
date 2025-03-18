ebb45d3587b1623a8ca62b14298cbf569fba49ca
Thomas Lawton <tlawton609@gmail.com>
2025-03-18 14:56:18 -0500

    New branch for DepthIMU board development

---
+++ src/apps/bm_devkit/DepthIMU_Test_1.0/user_code/user_code.cpp
@@ -0,0 +1,23 @@
+#include "user_code.h"
+#include "spotter.h"
+#include "pubsub.h"
+#include "bristlefin.h"
+#include "bsp.h"
+#include "debug.h"
+#include "lwip/inet.h"
+#include "sensors.h"
+#include "stm32_rtc.h"
+#include "task_priorities.h"
+#include "uptime.h"
+#include "app_util.h"
+
+#define LED_ON_TIME_MS 20
+#define LED_PERIOD_MS 1000
+
+void setup(void) {
+  /* USER ONE-TIME SETUP CODE GOES HERE */
+}
+
+void loop(void) {
+  /* USER LOOP CODE GOES HERE */
+}
