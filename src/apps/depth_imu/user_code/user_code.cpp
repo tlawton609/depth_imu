c9a210bb6eeb98d750555422b29d4e38d1ba7277
Victor Sowa <victor.sowa@sofarocean.com>
2025-06-09 23:15:40 -0700

    calculate the depth and add todo to pub/sub to other things

--- src/apps/depth_imu/user_code/user_code.cpp
+++ src/apps/depth_imu/user_code/user_code.cpp
@@ -2,6 +2,7 @@
 #include "spotter.h"
 #include "pubsub.h"
 #include "bsp.h"
+#include "configuration.h"
 #include "debug.h"
 #include "lwip/inet.h"
 #include "sensors.h"
@@ -13,17 +14,31 @@
 #include "sh2.h"
 #include "pa7ld.h"
 
+
+double gravity = 9.80665; // m/s^2
+double rho_salt = 1023.6; // kg/m^3
+double rho_fresh =  997.0474; // kg/m^3
+
+#define PRESSURE_IN_PASCALS(pressure) ((pressure * 100000))
+static constexpr char FRESH_WATER_FLAG[]  = "fresh_water";
+static constexpr uint8_t DEPTH_SENSOR_ADDR = 0x40;
+
+static bool fresh_water = false;
+
 sh2_Hal_t sh2_hal_driver;
 
 static Bno085 bno085_imu(&i2c1, IMU_ADDR);
 
 // TODO - create a #define or constant somewhere for the depth address
-static PA7LD depth_sensor(&i2c1, 0x40);
+static PA7LD depth_sensor(&i2c1, DEPTH_SENSOR_ADDR);
 
 void setup(void) {
   /* USER ONE-TIME SETUP CODE GOES HERE */
   vTaskDelay(10000);
   depth_sensor.init();
+
+  get_config_bool(BM_CFG_PARTITION_SYSTEM, FRESH_WATER_FLAG, strlen(FRESH_WATER_FLAG), &fresh_water);
+
 }
 
 void loop(void) {
@@ -35,10 +50,17 @@ void loop(void) {
     prev_time_ms = curr_time_ms;
     float pressure, temp;
     depth_sensor.readPTRaw(pressure, temp);
-    printf("pressure: %f bar, temp: %f °C\n", pressure, temp);
-    depth_sensor.readPTRaw(pressure, temp);
-    printf("pressure: %f bar, temp: %f °C\n", pressure, temp);
-    depth_sensor.readPTRaw(pressure, temp);
-    printf("pressure: %f bar, temp: %f °C\n", pressure, temp);
+    double depth = 0.0;
+    if (fresh_water) {
+      depth = PRESSURE_IN_PASCALS(pressure) / (gravity * rho_fresh);
+    } else {
+      depth = PRESSURE_IN_PASCALS(pressure) / (gravity * rho_salt);
+    }
+
+    printf("pressure: %f bar, temp: %f °C, depth: %fm\n", pressure, temp, depth);
+
+    // TODO - publis the depth on the BM bus so we can use it to make decisions in other systems?
+    // Or we subscribe to other systems and tell them what to do to make sure we are correct here?
+
   }
 }
